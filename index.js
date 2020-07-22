0dbcd79f30992fb9a5bdb8671777a99702be9fde
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-22 17:00:26 +0000

    Challenges - Arrays

--- index.js
+++ index.js
@@ -188,3 +188,88 @@ function extractCurrencyValue(str) {
 }
 
 console.log( extractCurrencyValue('$120') )
+
+//July 22, 2020 - Arrays
+
+//Challenge 1 - Is array copied?
+// What is this code going to show?
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+alert( fruits.length ); // ?
+
+//Challenge 2 - Array operations.
+
+// Let’s try 5 array operations.
+
+// Create an array styles with items “Jazz” and “Blues”.
+// Append “Rock-n-Roll” to the end.
+// Replace the value in the middle by “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+// Strip off the first value of the array and show it.
+// Prepend Rap and Reggae to the array.
+// The array in the process:
+
+// Jazz, Blues
+// Jazz, Blues, Rock-n-Roll
+// Jazz, Classics, Rock-n-Roll
+// Classics, Rock-n-Roll
+// Rap, Reggae, Classics, Rock-n-Roll
+
+//Solution
+
+let styles = ["Jazz", "Blues"]
+
+//Append "Rock-n-Roll" to the end with push
+styles.push("Rock-n-Roll")
+alert(styles)
+
+//Replace value in the middle with "Classics"
+let middle = Math.floor(styles.length / 2);
+styles[middle] = "Classics";
+alert(styles)
+
+/// Strip (Shift) off the first value of the array and show it.
+alert(styles.shift())
+alert(styles)
+
+// Prepend Rap and Reggae to the array.
+styles.unshift("Rap", "Reggae")
+alert(styles)
+
+//Challenge 4 - Sum input numbers
+
+// Write the function sumInput() that:
+
+// Asks the user for values using prompt and stores the values in the array.
+// Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// Calculates and returns the sum of array items.
+// P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+//Solution - While Loop, break, null, and For .. of
+function sumInput() {
+
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("A number please?", 0);
+
+    // should we cancel?
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
