29786c7453d8b446d8fb4c1fed60ea7e2188b47a
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-24 02:53:01 +0000

    Challenge - Array method camelcase

--- index.js
+++ index.js
@@ -344,3 +344,27 @@ alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
 alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
 alert( getMaxSubSum([1, 2, 3]) ); // 6
 alert( getMaxSubSum([-1, -2, -3]) ); // 0
+
+
+//July 23, 2020 - Array Methods
+
+// Challenge 1 - Translate border-left-width to borderLeftWidth
+
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+//camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+function camelize(str) {
+  return str
+  .split('-')
+  .map( (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1) )
+  .join('')
+}
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
